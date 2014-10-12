3d997a46ec30dfbe75c2025e7c26836fa19a46c7
Zach <zach@demandramp.com>
2014-10-12 12:19:59 -0700

    initial commit

--- themes/elsasam/parts/header.php
+++ themes/elsasam/parts/header.php
@@ -16,46 +16,12 @@
 </div>
 <![endif]-->    
 
-<?php
-/*
-Upper menubar (at screen top)
-=============================
-Delete this whole <nav>...</nav> block if you don't want it, and delete the line in func/navbar.php that looks like this:
-register_nav_menu('upper-bar', __('Screen-top menu'));
-*/
-?>
-<nav class="navbar navbar-default navbar-static-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".upper-navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
-    </div><!-- /.navbar-header -->
-    <div class="collapse navbar-collapse upper-navbar">    
-      <?php				
-          $args = array(
-            'theme_location' => 'upper-bar',
-            'depth' => 0,
-            'container'	=> false,
-            'fallback_cb' => false,
-            'menu_class' => 'nav navbar-nav',
-            'walker' => new BootstrapNavMenuWalker()
-          );
-          wp_nav_menu($args);
-      ?>
-	  </div><!-- /.navbar-collapse -->
-  </div>
-</nav>
 
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
         <h1 id="site-title">
-          <a class="text-muted" href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+          <a class="text-hide" href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
         </h1>
       </div>
     </div>

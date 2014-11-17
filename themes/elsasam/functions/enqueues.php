23421066b2ad9cf99b2e91c572d9530e52a7455c
Zach <zach@demandramp.com>
2014-11-17 15:24:03 -0800

    misc

--- themes/elsasam/functions/enqueues.php
+++ themes/elsasam/functions/enqueues.php
@@ -5,12 +5,12 @@ Google CDN jQuery with a local fallback
 See http://www.wpcoke.com/load-jquery-from-cdn-with-local-fallback-for-wordpress/
 */
 if( !is_admin()){ 
-    $url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
+    $url = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'; 
     $test_url = @fopen($url,'r'); 
     if($test_url !== false) { 
         function load_external_jQuery() {
             wp_deregister_script('jquery'); 
-            wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
+            wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); 
             wp_enqueue_script('jquery'); 
         }
         add_action('wp_enqueue_scripts', 'load_external_jQuery'); 
